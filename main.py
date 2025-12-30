8746a601b8ba13460c59b12c5b766de2da7f19dd
GOURAVsahu750 <gouravsahu750@gmail.com>
2025-12-31 00:17:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,31 @@
 from telethon import TelegramClient, events
+from telethon.sessions import StringSession
 import time
-import os
 
-# ===== APNI DETAILS =====
-api_id = 32557753              # <-- apna API ID
-api_hash = "3aec7775e6af24432f2414f941409876"     # <-- apna API HASH
-session_name = "titan_userbot"
-# ========================
+api_id = 32557753
+api_hash = "3aec7775e6af24432f2414f941409876"
+STRING_SESSION = "1BVtsOKYBuzSXbkJ15Yjx9HhYTF8Frf5K1W3jFVFA1kW064TjA_Twb8VkAczmuzVLTh_Y0We9Mvf0lWsalFPjxdrPs6KtLN9EvJwcbW81w9svCSazjRnDhWxPYZ44-rrhXl1Lh8mAGL8L4NwM-tmm_Gyrfuk7blSPtOYJYCXKg6uKmPKKwkEhwvTgGj-eQIqgFYgHLZZ4zM8t_18qRtMcx2uL5K06hebzd-zVoeYhdqsTbDbIiVi_2cLJehskL1tHU73oSsqH4gN3ExF5rvgvcT3wBtO9u1xD7kDwpOVAUwvwPwvk2XjnISTqvK_zcDZL0gfNVHdjuk71ksUVmhGossJiY4Rx4yw="
 
-client = TelegramClient(session_name, api_id, api_hash)
+client = TelegramClient(StringSession(STRING_SESSION), api_id, api_hash)
 
-IDLE_LIMIT = 5  # seconds (5 sec idle = offline)
+IDLE_LIMIT = 5
 last_active_time = time.time()
 replied_users = set()
 
-# -----------------------------
-# Track your own activity
-# -----------------------------
 @client.on(events.NewMessage(from_users='me'))
 async def track_activity(event):
     global last_active_time, replied_users
     last_active_time = time.time()
     replied_users.clear()
 
-# -----------------------------
-# Auto reply ONLY in DM
-# -----------------------------
 @client.on(events.NewMessage(incoming=True))
 async def auto_reply_dm(event):
-    if not event.is_private:
+    if not event.is_private or event.out:
         return
 
-    if event.out:
+    if time.time() - last_active_time < IDLE_LIMIT:
         return
 
-    idle_time = time.time() - last_active_time
-
-    # Agar online ho (5 sec se kam idle)
-    if idle_time < IDLE_LIMIT:
-        return
-
-    # Same user ko baar-baar reply na ho
     if event.sender_id in replied_users:
         return
 
@@ -52,6 +37,5 @@ async def auto_reply_dm(event):
         "Aɴᴅ Rᴇᴩʟy Yᴏᴜ 😴😴"
     )
 
-print("🚀 Telegram Userbot started on Railway")
 client.start()
 client.run_until_disconnected()
